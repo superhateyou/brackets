2111c31db1c70e54eb801f1dda900aa8f8c68d8f
Pavel Serzhan <p.i.serzhan@gmail.com>
2020-10-25 21:56:59 +0300

    feat: brackets task complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const inputBrackets = str.split('');
+    let bracketStack = [];
+    const openBrackets = [];
+    const closeBrackets = [];
+    bracketsConfig.forEach(x => {
+        openBrackets.push(x[0]);
+        closeBrackets.push(x[1]);
+    });
+
+    for (let i = 0; i < inputBrackets.length; i++) {
+        if (openBrackets.some(x => x === inputBrackets[i])) {
+            if (closeBrackets.some(x => x === inputBrackets[i])) {
+                if (bracketStack[bracketStack.length - 1] === inputBrackets[i]) {
+                    bracketStack.pop();
+                } else {
+                    bracketStack.push(inputBrackets[i])
+                }
+            } else {
+                bracketStack.push(inputBrackets[i])
+            }
+        } else if (closeBrackets.some(x => x === inputBrackets[i])) {
+            if (bracketStack[bracketStack.length - 1] === openBrackets[closeBrackets.indexOf(inputBrackets[i])]) {
+                bracketStack.pop();
+            } else {
+                return false;
+            }
+        }
+    }
+
+    if (bracketStack.length > 0) 
+        return false
+    else return true;
 }
